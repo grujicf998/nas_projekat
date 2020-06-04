7addbc4a614b58b91f69cb6f5dd824b9cdd801af
grujicf998 <grujicf998@gmail.com>
2020-06-04 23:07:03 +0200

    First commit

---
+++ radni_projekat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace radni_projekat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
